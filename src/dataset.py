7cff566ab4983ecb6d7c3e86a4838fa7570009ae
Rafael Goncalves <rafaelgoncalves@riseup.net>
2020-06-20 16:12:10 -0300

    Add validation dataset

--- src/dataset.py
+++ src/dataset.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset
 
 class MyDataset(Dataset):
+    """Training dataset."""
     def __init__(self, triples, queries, docs, tokenizer, max_length):
         self.triples = triples
         self.queries = queries
@@ -26,6 +27,38 @@ class MyDataset(Dataset):
                 p_tok, p_mask, p_type,
                 n_tok, n_mask, n_type,
                 query, doc_pos, doc_neg)
+
+    def tokenize(self, text):
+        tokens = self.tokenizer.encode_plus(text=text, max_length=self.max_length,
+                                       pad_to_max_length=True, add_special_tokens = True,
+                                       return_tensors='pt')
+        return tokens["input_ids"], tokens['attention_mask'], tokens['token_type_ids']
+
+class ValDataset(Dataset):
+    """Validation dataset."""
+    def __init__(self, queries, docs, tokenizer, max_length, queries_1000):
+        self.queries = queries
+        self.docs = docs
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.val_data = [(q, doc_ids) for q, doc_ids in queries_1000.items()]
+
+    def __len__(self):
+        return len(self.val_data)
+
+    def __getitem__(self, idx):
+        qid, doc_ids = self.val_data[idx]
+        query = self.queries[qid]
+        q_tok, q_mask, q_type = self.tokenize(query)
+        docs2query = [self.docs[doc_id] for doc_id in doc_ids]
+        tokens = self.tokenizer.batch_encode_plus(batch_text_or_text_pairs=docs2query, max_length=self.max_length,
+                                       pad_to_max_length=True, add_special_tokens = True,
+                                       return_tensors='pt')
+        docs_tok = tokens["input_ids"]
+        docs_mask = tokens["attention_mask"]
+        docs_type = tokens["token_type_ids"]
+
+        return  (q_tok, q_mask, q_type, docs_tok, docs_mask, docs_type, qid, doc_ids)
 
     def tokenize(self, text):
         tokens = self.tokenizer.encode_plus(text=text, max_length=self.max_length,
