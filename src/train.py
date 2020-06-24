a575ae5f7414f01ab00675245f4bfc8e6b935eba
Rafael Goncalves <rafaelgoncalves@riseup.net>
2020-06-24 18:17:55 -0300

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,16 @@
+from argparse import ArgumentParser
+from model import TwoTower
+
+def main(args):
+    trainer = Trainer.from_argparse_args(args)
+    dict_args = vars(args)
+    model = TwoTower(**dict_args)
+    trainer.fit(model)
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser = TwoTower().add_model_specific_args(parser)
+    parser = Trainer.add_argparse_args(parser)
+    args = parser.parse_args()
+
+    main(args)
